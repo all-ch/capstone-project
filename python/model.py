34490190b4f7f43e34a4b5e12f45b2383f939677
allch <229534698+all-ch@users.noreply.github.com>
2026-04-22 19:22:36 -0700

    anchor words->phrases, add axis calculation

--- python/model.py
+++ python/model.py
@@ -1,5 +1,28 @@
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
+from torch import Tensor
+import numpy as np
+import pandas as pd
+
+
+def get_anchor_embeds(location: str, model: SentenceTransformer) -> Tensor | np.ndarray:
+    return model.encode(pd.read_csv(location, header=None)[0].tolist())
+
+
+def avg_vec(embeddings: Tensor | np.ndarray) -> Tensor | np.ndarray:
+    return np.mean(embeddings, axis=0)
+
+
+def get_anchor_axis(
+    pos_embeds: Tensor | np.ndarray, neg_embeds: Tensor | np.ndarray
+) -> Tensor | np.ndarray:
+    return avg_vec(pos_embeds) - avg_vec(neg_embeds)
 
 
 if __name__ == "__main__":
-    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+
+    religion_neg = get_anchor_embeds("../data/religion_neg.csv", model)
+    religion_pos = get_anchor_embeds("../data/religion_pos.csv", model)
+    religion_axis = get_anchor_axis(religion_pos, religion_neg)
+
+    print(religion_axis, religion_axis.shape)
