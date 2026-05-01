9edb34ef3ab42f1ac22a23677e15d1b6c83ccfba
allch <229534698+all-ch@users.noreply.github.com>
2026-05-01 11:27:19 -0700

    added pca function

--- python/pca.py
+++ python/pca.py
@@ -1,9 +1,28 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from torch import Tensor
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import embeddings
+
+
+def get_scores(
+    scalar: StandardScaler,
+    n: int,
+    pos_vec: Tensor | np.ndarray,
+    neg_vec: Tensor | np.ndarray,
+    axis: Tensor | np.ndarray,
+    pos_embeds: Tensor | np.ndarray,
+    neg_embeds: Tensor | np.ndarray,
+) -> tuple[PCA, np.ndarray]:
+    x_combined = np.vstack(
+        (axis.reshape(1, -1), pos_vec, neg_vec, pos_embeds, neg_embeds)
+    )
+    x_scaled = scalar.fit_transform(x_combined)
+    pca = PCA(n_components=n)
+    scores = pca.fit_transform(x_scaled)
+    return (pca, scores)
 
 
 if __name__ == "__main__":
@@ -26,14 +45,14 @@ if __name__ == "__main__":
     rand_embeds = embeddings.get_speech_embeds(nlp, model, data, "Akira Morita", 1997)
 
     # PCA
-    x_combined = np.vstack(
-        (
-            religion_axis.reshape(1, -1),
-            religion_pos,
-            religion_neg,
-            religious_embeds,
-            rand_embeds,
-        )
+    pca, scores = get_scores(
+        scalar,
+        2,
+        religion_pos,
+        religion_neg,
+        religion_axis,
+        religious_embeds,
+        rand_embeds,
     )
     labels = np.array(
         ["Aggregate Religion Vertex"] * 1
@@ -42,10 +61,6 @@ if __name__ == "__main__":
         + ["Religious Text Example"] * religious_embeds.shape[0]
         + ["Random Text Example"] * rand_embeds.shape[0]
     )
-    x_scaled = scalar.fit_transform(x_combined)
-
-    pca = PCA(n_components=2)
-    scores = pca.fit_transform(x_scaled)
 
     # plot
     plt.figure(figsize=(10, 7))
@@ -62,5 +77,5 @@ if __name__ == "__main__":
     plt.ylabel(f"PC2 ({pca.explained_variance_ratio_[1]:.1%} variance)", fontsize=12)
     plt.grid(True, linestyle="--", alpha=0.6)
 
-    # plt.savefig("visual/pca.png", dpi=300, bbox_inches="tight")
-    plt.show()
+    plt.savefig("visual/pca.png", dpi=300, bbox_inches="tight")
+    plt.close()
