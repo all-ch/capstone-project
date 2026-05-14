0d5858538af2628e1e26a245871ed8bf23597db0
allch <229534698+all-ch@users.noreply.github.com>
2026-05-13 18:38:47 -0700

    add to gitignore

--- python/model.py
+++ python/model.py
@@ -2,17 +2,20 @@
 # and comparing the distribution of sentence-level topic scores between different speeches.
 
 # Natural Language Processing and Text Analysis for Conference Speeches
-from sentence_transformers import SentenceTransformer # For converting sentences into numerical embeddings
-from spacy.language import Language # For natural language processing tasks such as sentence tokenization
+from sentence_transformers import (
+    SentenceTransformer,
+)  # For converting sentences into numerical embeddings
+from spacy.language import (
+    Language,
+)  # For natural language processing tasks such as sentence tokenization
 
 # Mathematical and data manipulation libraries
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np # For numerical operations
-from torch import Tensor # For handling numerical data in tensor format
+import numpy as np  # For numerical operations
+from torch import Tensor  # For handling numerical data in tensor format
 
 # Data handling
-import pandas as pd # for working with dataframes
-import numpy as np # For numerical operations
+import pandas as pd  # for working with dataframes
 
 # Plotting libraries
 import matplotlib.pyplot as plt
@@ -21,34 +24,46 @@ import matplotlib.axes as axes
 # Custom module for handling embeddings
 from python import embeddings
 
-import seaborn as sns # For advanced data visualization, used for creating violin plots and enhancing the aesthetics of plots
+import seaborn as sns  # For advanced data visualization, used for creating violin plots and enhancing the aesthetics of plots
 
-def compute_speech_topic_score(sentence_embeddings: np.ndarray | Tensor, topic_vector: np.ndarray | Tensor, q: float = 0.75) -> float:
+
+def compute_speech_topic_score(
+    sentence_embeddings: np.ndarray | Tensor,
+    topic_vector: np.ndarray | Tensor,
+    q: float = 0.75,
+) -> float:
     """
     Computes the average cosine similarity between sentence embeddings and a topic vector.
 
     Args:
         sentence_embeddings (np.ndarray | Tensor): The numerical representations of the sentences in a speech, typically of shape (num_sentences, embedding_dim).
         topic_vector (np.ndarray | Tensor): A numerical representation of the topic, typically of shape (embedding_dim,) creagted by averaging the embeddings of keywords related to the topic (created from init_speech_embeds).
-        q (float): The quantile to use for computing the topic score, with a default value of 0.75. 
+        q (float): The quantile to use for computing the topic score, with a default value of 0.75.
         - This parameter allows for a more robust topic score by considering the distribution of sentence-level scores and focuses on the upper quantile.
     Returns:
         float: The average topic score for the speech, computed as the specified quantile of the cosine similarity scores between the sentence embeddings and the topic vector.
     """
     # NOTE: Old implementation
-    #return cosine_similarity(sentence_embeddings, topic_vector.reshape(1, -1)).mean()
+    # return cosine_similarity(sentence_embeddings, topic_vector.reshape(1, -1)).mean()
 
-    sent_scores = cosine_similarity(sentence_embeddings, topic_vector.reshape(1, -1)).flatten()
+    sent_scores = cosine_similarity(
+        sentence_embeddings, topic_vector.reshape(1, -1)
+    ).flatten()
     return float(np.quantile(sent_scores, q))
 
 
-
-def compute_yearly_topic_scores(conference_data: pd.DataFrame, topic_vector: np.ndarray | Tensor, nlp: Language, model: SentenceTransformer, q: float = 0.75) -> tuple[dict, dict]:
+def compute_yearly_topic_scores(
+    conference_data: pd.DataFrame,
+    topic_vector: np.ndarray | Tensor,
+    nlp: Language,
+    model: SentenceTransformer,
+    q: float = 0.75,
+) -> tuple[dict, dict]:
     """
     Computes the average topic score for each year in the conference data.
-    
+
     This function analyzes the speeches given in each year of the conference by computing the average cosine similarity between
-    the sentence embeddings of the speeches and a given topic vector. The result is a dictionary of the format 
+    the sentence embeddings of the speeches and a given topic vector. The result is a dictionary of the format
     {year: average_topic_score}, where the average topic score is computed as the mean of the cosine similarities for all speeches given in that year.
 
     Args:
@@ -62,7 +77,7 @@ def compute_yearly_topic_scores(conference_data: pd.DataFrame, topic_vector: np.
         tuple[dict, dict]: A tuple containing two dictionaries:
             - The first dictionary maps each year to a list of topic scores for the speeches given in that year, with the format {year: [topic_score1, topic_score2, ...]}.
             - The second dictionary maps each year to the average topic score for that year, with the format {year: average_topic_score}, where the average topic score is computed as the mean of the topic scores for all speeches given in that year.
-    """ 
+    """
 
     yearly_topic_avg_score = {}
     yearly_topic_scores = {}
@@ -73,7 +88,7 @@ def compute_yearly_topic_scores(conference_data: pd.DataFrame, topic_vector: np.
                 lambda x: compute_speech_topic_score(
                     embeddings.get_sent_embeds(embeddings.split_speech(x, nlp), model),
                     topic_vector,
-                    q=q
+                    q=q,
                 )
             )
             .to_list()
@@ -83,7 +98,9 @@ def compute_yearly_topic_scores(conference_data: pd.DataFrame, topic_vector: np.
     return yearly_topic_scores, yearly_topic_avg_score
 
 
-def compute_sent_level_topic_score_dist(speech_embeddings: Tensor | np.ndarray, topic_vector: Tensor | np.ndarray) -> list:
+def compute_sent_level_topic_score_dist(
+    speech_embeddings: Tensor | np.ndarray, topic_vector: Tensor | np.ndarray
+) -> list:
     """
     Computes the distribution of sentence level topic scores for a given speech.
 
@@ -96,7 +113,7 @@ def compute_sent_level_topic_score_dist(speech_embeddings: Tensor | np.ndarray, 
         list: A list of cosine similarity scores for each sentence in the speech with respect to the topic.
             - Each score represents how closely a sentence aligns with the topic, with higher scores indicating
     """
-    
+
     sent_scores = []
     for sent_embedding in speech_embeddings:
         sent_topic_score = cosine_similarity(
@@ -144,12 +161,20 @@ def save_topic_score_by_year_plot(topic: str, yearly_scores: dict) -> None:
     ax.set_xlabel("Year")
     ax.set_ylabel(f"Average {topic} Topic Score")
     ax.set_title(f"Average {topic} Topic Score by Year")
-    #plt.savefig(f"outputs/plots/yearly_{topic}_scores.png", dpi=300, bbox_inches="tight") # for og data
-    plt.savefig(f"outputs/plots/yearly_{topic}_scores.png", dpi=300, bbox_inches="tight")
+    # plt.savefig(f"outputs/plots/yearly_{topic}_scores.png", dpi=300, bbox_inches="tight") # for og data
+    plt.savefig(
+        f"outputs/plots/yearly_{topic}_scores.png", dpi=300, bbox_inches="tight"
+    )
 
     plt.close()
 
-def conf_boxplot(topic: str, yearly_scores: dict, show_trend: bool = True, trend_method: str = "median") -> None:
+
+def conf_boxplot(
+    topic: str,
+    yearly_scores: dict,
+    show_trend: bool = True,
+    trend_method: str = "median",
+) -> None:
     """
     Creates a boxplot of topic scores by year for a given topic.
 
@@ -177,8 +202,8 @@ def conf_boxplot(topic: str, yearly_scores: dict, show_trend: bool = True, trend
         elif trend_method == "mean":
             trend_values = [np.mean(yearly_scores[year]) for year in years]
         else:
-            pass #TODO: more trend methods?
-        if (trend_method in ["median", "mean"]):
+            pass  # TODO: more trend methods?
+        if trend_method in ["median", "mean"]:
             m, b = np.polyfit(years, trend_values, 1)
             ax.plot(
                 years,
@@ -195,13 +220,25 @@ def conf_boxplot(topic: str, yearly_scores: dict, show_trend: bool = True, trend
     ax.set_ylabel(f"{topic} Topic Scores")
     ax.set_title(f"{topic} Topic Scores by Year")
     ax.legend()
-    #plt.savefig(f"outputs/plots/boxplot_yearly_{topic}_scores.png", dpi=300, bbox_inches="tight") # for og data
-    plt.savefig(f"outputs/plots/boxplot_yearly_{topic}_scores_quantile.png", dpi=300, bbox_inches="tight")
+    # plt.savefig(f"outputs/plots/boxplot_yearly_{topic}_scores.png", dpi=300, bbox_inches="tight") # for og data
+    plt.savefig(
+        f"outputs/plots/boxplot_yearly_{topic}_scores_quantile.png",
+        dpi=300,
+        bbox_inches="tight",
+    )
 
     plt.close()
 
 
-def conf_hist_plot(topic: str, title: str, speaker: str, ax: axes.Axes, axis: Tensor | np.ndarray, embeds: Tensor | np.ndarray, color: str, ) -> None:
+def conf_hist_plot(
+    topic: str,
+    title: str,
+    speaker: str,
+    ax: axes.Axes,
+    axis: Tensor | np.ndarray,
+    embeds: Tensor | np.ndarray,
+    color: str,
+) -> None:
     """
     Creates a histogram plot of sentence-level topic scores for a given speech.
 
@@ -213,7 +250,7 @@ def conf_hist_plot(topic: str, title: str, speaker: str, ax: axes.Axes, axis: Te
         title (str): The title of the plot.
         speaker (str): The name of the speaker whose speech is being plotted.
         ax (axes.Axes): The matplotlib axes object on which to create the histogram.
-        axis (Tensor | np.ndarray): The topic vector used to compute the sentence-level topic scores, typically of shape (embedding_dim,).        
+        axis (Tensor | np.ndarray): The topic vector used to compute the sentence-level topic scores, typically of shape (embedding_dim,).
         embeds (Tensor | np.ndarray): The numerical representations of the sentences in the speech, typically of shape (num_sentences, embedding_dim).
         color (str): The color to use for the histogram bars, specified as a string (e.g., "cornflowerblue", "coral").
     Returns:
@@ -235,7 +272,10 @@ def conf_hist_plot(topic: str, title: str, speaker: str, ax: axes.Axes, axis: Te
     ax.grid(True, alpha=0.3)
     ax.set_xlim(-0.5, 0.5)
 
-def conf_violin_plot_yearly(topic: str, yearly_scores: dict, target_year: int, color: str) -> None:
+
+def conf_violin_plot_yearly(
+    topic: str, yearly_scores: dict, target_year: int, color: str
+) -> None:
     """
     Creates a violin plot showing the distribution of all speech scores for a specific year.
     """
@@ -243,18 +283,18 @@ def conf_violin_plot_yearly(topic: str, yearly_scores: dict, target_year: int, c
     if target_year not in yearly_scores:
         print(f"Year {target_year} not found in yearly_scores.")
         return
-        
-    year_data = yearly_scores[target_year] #
+
+    year_data = yearly_scores[target_year]  #
 
     # 2. Setup the plot
     fig, ax = plt.subplots(figsize=(8, 6))
-    
+
     # 3. Create the violin plot
     # This visualizes the density and range of all speech scores in that year
     sns.violinplot(x=year_data, ax=ax, color=color, inner="quartile")
 
     # 4. Add the 75th quantile marker of the yearly distribution
-    q75 = np.quantile(year_data, 0.75) #
+    q75 = np.quantile(year_data, 0.75)  #
     ax.axvline(
         float(q75),
         color="red",
@@ -268,13 +308,28 @@ def conf_violin_plot_yearly(topic: str, yearly_scores: dict, target_year: int, c
     ax.legend()
     ax.grid(True, alpha=0.3)
 
-    plt.savefig(f"outputs/plots/{topic}_{target_year}_yearly_violin.png", dpi=300, bbox_inches="tight")
+    plt.savefig(
+        f"outputs/plots/{topic}_{target_year}_yearly_violin.png",
+        dpi=300,
+        bbox_inches="tight",
+    )
     plt.close()
 
-def save_hist_comparison_plot( topic: str, neutral: str, topic_spkr: str, neutral_spkr: str, axis: Tensor | np.ndarray, topic_embeds: Tensor | np.ndarray, neutral_embeds: Tensor | np.ndarray, topic_color: str, neutral_color: str, ) -> None:
+
+def save_hist_comparison_plot(
+    topic: str,
+    neutral: str,
+    topic_spkr: str,
+    neutral_spkr: str,
+    axis: Tensor | np.ndarray,
+    topic_embeds: Tensor | np.ndarray,
+    neutral_embeds: Tensor | np.ndarray,
+    topic_color: str,
+    neutral_color: str,
+) -> None:
     """
     Saves a histogram comparison plot of sentence-level topic scores for a given topic and neutral speeches.
-    
+
     Args:
         topic (str): The name of the topic being plotted.
         neutral (str): The label for the neutral speech.
@@ -294,8 +349,12 @@ def save_hist_comparison_plot( topic: str, neutral: str, topic_spkr: str, neutra
 
     # Creating the histogram plots
     conf_hist_plot(topic, topic, topic_spkr, ax1, axis, topic_embeds, topic_color)
-    conf_hist_plot(topic, neutral, neutral_spkr, ax2, axis, neutral_embeds, neutral_color)
+    conf_hist_plot(
+        topic, neutral, neutral_spkr, ax2, axis, neutral_embeds, neutral_color
+    )
     plt.tight_layout()
-    plt.savefig(f"outputs/plots/{topic}_hist_comparison.png", dpi=300, bbox_inches="tight")
+    plt.savefig(
+        f"outputs/plots/{topic}_hist_comparison.png", dpi=300, bbox_inches="tight"
+    )
 
     plt.close()
