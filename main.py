9fc673649fef553a57d2fbdd35b1ffe1ca3255ac
allch <229534698+all-ch@users.noreply.github.com>
2026-05-13 19:12:09 -0700

    added the goat tyler violin copy paste edit

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 # This script serves as the main entry point for the project. It orchestrates the loading of models and data, the computation of topic scores, and the generation of visualizations for conference speeches on various topics.
 
 # Import necessary libraries and modules
-from sklearn.preprocessing import StandardScaler # For standardizing features by removing the mean and scaling to unit variance
-from python import embeddings # Custom module for handling sentence embeddings
-from python import model as tm # Custom module for computing topic scores and generating visualizations
-from python import pca # Custom module for performing Principal Component Analysis and generating PCA plots
+from sklearn.preprocessing import (
+    StandardScaler,
+)  # For standardizing features by removing the mean and scaling to unit variance
+from python import embeddings  # Custom module for handling sentence embeddings
+from python import (
+    model as tm,
+)  # Custom module for computing topic scores and generating visualizations
+from python import (
+    pca,
+)  # Custom module for performing Principal Component Analysis and generating PCA plots
 
-import pickle # for saving and loading computed scores
-import os # for checking if cache files exist
+import pickle  # for saving and loading computed scores
+import os  # for checking if cache files exist
 
 # Sentence embedding and natural language processing models
-EMBEDDINGS_MODEL = "sentence-transformers/all-mpnet-base-v2" # https://huggingface.co/sentence-transformers/all-mpnet-base-v2, a pre-trained model for generating sentence embeddings
-NLP_MODEL = "en_core_web_sm" # English model for natural language processing tasks such as sentence tokenization
+EMBEDDINGS_MODEL = "sentence-transformers/all-mpnet-base-v2"  # https://huggingface.co/sentence-transformers/all-mpnet-base-v2, a pre-trained model for generating sentence embeddings
+NLP_MODEL = "en_core_web_sm"  # English model for natural language processing tasks such as sentence tokenization
 
 # Conference speech data
 DATA_DIR = "data/processed/speeches.csv"
@@ -21,7 +27,7 @@ NEW_DATA_DIR = "data/processed/new_speeches.csv"
 # NOTE 1: Sentences were generated using ChatGPT, so they may not be perfect representations, and should be checked for accuracy and relevance to the topic.
 #  AI generated sentences should be further reviewed and validated.
 
-#NOTE 2: If wanting to add more anchors, create a new CSV file with the same format as existing anchor files (no header, sentences in the first column), then
+# NOTE 2: If wanting to add more anchors, create a new CSV file with the same format as existing anchor files (no header, sentences in the first column), then
 # add the file path to the appropriate topic in the TOPICS dictionary below, and ensure that the file is located in the correct directory.
 RELIGION_POS_DIR = "data/anchors/religion_pos_sentences.csv"
 RELIGION_NEG_DIR = "data/anchors/religion_neg_sentences.csv"
@@ -71,6 +77,7 @@ TOPICS = {
     },
 }
 
+
 # Main function to orchestrate the loading of models and data, computation of topic scores, and generation of visualizations for conference speeches on various topics.
 def main():
 
@@ -78,7 +85,7 @@ def main():
     print("loading all models and data...")
     model, data, nlp = embeddings.init_models(
         EMBEDDINGS_MODEL,
-        NEW_DATA_DIR, #DATA_DIR,
+        NEW_DATA_DIR,  # DATA_DIR,
         NLP_MODEL,
     )
     scalar = StandardScaler()
@@ -95,34 +102,37 @@ def main():
             TOPICS[topic]["Positive"], TOPICS[topic]["Negative"], model
         )
 
-
         # NOTE: cache system, however since it's inside the loop, it's going to ask for input for each topic
         cache_path = f"data/processed/{topic}_scores.pkl"
         update_scores = False
 
         # Check if the file exists first; if not, we HAVE to calculate
         if os.path.exists(cache_path):
-            update_scores = input(f"{topic} Cache found. Recompute scores? (Y/N): ").lower()
-            update_scores = (update_scores == 'y')
+            update_scores = input(
+                f"{topic} Cache found. Recompute scores? (Y/N): "
+            ).lower()
+            update_scores = update_scores == "y"
         else:
             print(f"No {topic} cache found. Computing scores..")
             update_scores = True
 
         if update_scores:
-            #NOTE: If wanting to change quantile, you're going to. need to recompute scores.
-            yearly_topic_scores, yearly_avg_score = tm.compute_yearly_topic_scores(data, topic_axis, nlp, model, q = 0.75) 
+            # NOTE: If wanting to change quantile, you're going to. need to recompute scores.
+            yearly_topic_scores, yearly_avg_score = tm.compute_yearly_topic_scores(
+                data, topic_axis, nlp, model, q=0.75
+            )
 
             # Save both dictionaries to one file
-            with open(cache_path, 'wb') as f:
+            with open(cache_path, "wb") as f:
                 pickle.dump((yearly_topic_scores, yearly_avg_score), f)
             print(f"Scores saved to {cache_path}")
 
         else:
             print(f"Loading {topic} scores")
-            with open(cache_path, 'rb') as f:
+            with open(cache_path, "rb") as f:
                 yearly_topic_scores, yearly_avg_score = pickle.load(f)
 
-        print(f"loading {topic} example speech embeddings...") 
+        print(f"loading {topic} example speech embeddings...")
         # The embeddings for the topic speech and the neutral speech are initialized based on the specified speakers and years for each topic. These embeddings will be used for generating visualizations and computing topic scores.
         topic_embeds, neutral_embeds = embeddings.init_speech_embeds(
             nlp,
@@ -136,39 +146,40 @@ def main():
 
         print(f"creating {topic} pca plot...")
         # PCA plot are generated to visualize distribution of sentence embeddings for topic speech in relation to topic axis.
-        pca.save_pca_plot(
-            topic, scalar, 2, pos_vec, neg_vec, topic_axis, topic_embeds, neutral_embeds
-        )
+        # pca.save_pca_plot(
+        #    topic, scalar, 2, pos_vec, neg_vec, topic_axis, topic_embeds, neutral_embeds
+        # )
         print(f"saved {topic} pca plot!")
 
         # NEW: boxplot
         print(f"creating {topic} topic scores by year boxplot...")
-        tm.conf_boxplot(topic, yearly_topic_scores, show_trend=True, trend_method="mean")  
+        # tm.conf_boxplot(topic, yearly_topic_scores, show_trend=True, trend_method="mean")
 
         print(f"saved {topic} topic scores by year plot!")
 
         #
-        tm.conf_violin_plot_yearly(topic, yearly_topic_scores, 1999, "cornflowerblue")
-        tm.conf_violin_plot_yearly(topic, yearly_topic_scores, 2007, "cornflowerblue")
-        tm.conf_violin_plot_yearly(topic, yearly_topic_scores, 2012, "cornflowerblue")
-        tm.conf_violin_plot_yearly(topic, yearly_topic_scores, 2015, "cornflowerblue")
+        # tm.conf_violin_plot_yearly(topic, yearly_topic_scores, 1999, "cornflowerblue")
+        # tm.conf_violin_plot_yearly(topic, yearly_topic_scores, 2007, "cornflowerblue")
+        # tm.conf_violin_plot_yearly(topic, yearly_topic_scores, 2012, "cornflowerblue")
+        # tm.conf_violin_plot_yearly(topic, yearly_topic_scores, 2015, "cornflowerblue")
 
         print(f"creating {topic} histogram comparison plot...")
         # Creating histogram comparison plots comparing the distribution of sentence-level topic scores between a specified topic speech and specified neutral speech.
-        tm.save_hist_comparison_plot(
-            topic,
-            "Neutral",
-            TOPICS[topic]["Speaker"],
-            TOPICS["Neutral"]["Speaker"],
-            topic_axis,
-            topic_embeds,
-            neutral_embeds,
-            "cornflowerblue",
-            "coral",
-        )
+        # tm.save_hist_comparison_plot(
+        #   topic,
+        #   "Neutral",
+        #    TOPICS[topic]["Speaker"],
+        #   TOPICS["Neutral"]["Speaker"],
+        #  topic_axis,
+        #  topic_embeds,
+        #  neutral_embeds,
+        #   "cornflowerblue",
+        #    "coral",
+        # )
+        print("tyler the goat")
+        tm.the_goat_tyler(topic, yearly_topic_scores)
 
     print("script finished.")
-
 
 
 # Running this script will execute the main function, which will load the necessary models and data, compute topic scores for the specified topics, and generate visualizations for each topic.
