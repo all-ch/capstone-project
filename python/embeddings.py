4f83e11fd7ee2fc67395709bf12d59e34ded7af4
allch <229534698+all-ch@users.noreply.github.com>
2026-05-29 14:31:11 -0700

    updated embeddings.py

--- python/embeddings.py
+++ python/embeddings.py
@@ -1,89 +1,93 @@
 from sentence_transformers import SentenceTransformer
+from sklearn.mixture import GaussianMixture
 from spacy.language import Language
-import spacy
 from pathlib import Path
 from torch import Tensor
 import pandas as pd
 import numpy as np
+import spacy
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
+# NOTE: base functions
 
-def get_anchor_embeds(
+
+def calculate_average_vector(embeddings: Tensor | np.ndarray) -> Tensor | np.ndarray:
+    return np.mean(embeddings, axis=0)
+
+
+def calculate_sentence_embeddings(
+    sent: list[str], model: SentenceTransformer
+) -> Tensor | np.ndarray:
+    return model.encode(sent)
+
+
+# NOTE: anchor functions
+
+
+def calculate_anchor_embeddings(
     location: str | Path, model: SentenceTransformer
 ) -> Tensor | np.ndarray:
     return model.encode(pd.read_csv(location, header=None, sep="\t")[0].tolist())
 
 
-def get_sent_embeds(sent: list[str], model: SentenceTransformer) -> Tensor | np.ndarray:
-    return model.encode(sent)
-
-
-def avg_vec(embeddings: Tensor | np.ndarray) -> Tensor | np.ndarray:
-    return np.mean(embeddings, axis=0)
-
-
-def get_anchor_axis(
-    pos_embeds: Tensor | np.ndarray, neg_embeds: Tensor | np.ndarray
+def calculate_topic_axis(
+    positive_vector: Tensor | np.ndarray, negative_vector: Tensor | np.ndarray
 ) -> Tensor | np.ndarray:
-    return avg_vec(pos_embeds) - avg_vec(neg_embeds)
+    return positive_vector - negative_vector
 
 
-def split_speech(text: str, nlp: Language) -> list[str]:
-    doc = nlp(text)
-    return [sent.text.strip() for sent in doc.sents]
+def calculate_topic_offset(
+    positive_vector: Tensor | np.ndarray, negative_vector: Tensor | np.ndarray
+) -> Tensor | np.ndarray:
+    return (positive_vector + negative_vector) / 2
 
 
-def find_speech(data: pd.DataFrame, speaker: str, cyear: int | None = None) -> str:
-    speech = (
-        data[data.speaker == speaker]["speech"]
-        if cyear is None
-        else data[(data.speaker == speaker) & (data.cyear == cyear)]["speech"]
+def calculate_topic_vectors(
+    positive_anchor_file_location: str | Path,
+    negative_anchor_file_location: str | Path,
+    embeddings_model: SentenceTransformer,
+) -> list[Tensor | np.ndarray]:
+    positive_anchor_dataframe = ROOT_DIR / positive_anchor_file_location
+    negative_anchor_dataframe = ROOT_DIR / negative_anchor_file_location
+
+    positive_anchor_embeddings = calculate_anchor_embeddings(
+        positive_anchor_dataframe, embeddings_model
     )
-    return speech.item()
+    negative_anchor_embeddings = calculate_anchor_embeddings(
+        negative_anchor_dataframe, embeddings_model
+    )
+
+    topic_axis = calculate_topic_axis(
+        positive_anchor_embeddings, negative_anchor_embeddings
+    )
+    topic_offset = calculate_topic_offset(
+        positive_anchor_embeddings, negative_anchor_embeddings
+    )
+    return [topic_axis, topic_offset]
 
 
-def get_speech_embeds(
-    nlp: Language,
-    model: SentenceTransformer,
-    data: pd.DataFrame,
-    speaker: str,
-    cyear: int | None = None,
-) -> Tensor | np.ndarray:
-    return get_sent_embeds(split_speech(find_speech(data, speaker, cyear), nlp), model)
+# NOTE: speech functions
 
 
-def init_models(
-    model_name: str, data_file: str | Path, nlp_name: str
-) -> tuple[SentenceTransformer, pd.DataFrame, Language]:
-    data_file = ROOT_DIR / data_file
+def split_speech_into_sentences(text: str, nlp: Language) -> list[str]:
+    speech = nlp(text)
+    return [sentence.text.strip() for sentence in speech.sents]
+
+
+# NOTE: initialization functions
+
+
+def initialize_all_models(
+    transformer_model_name: str,
+    data_file_location: str | Path,
+    nlp_name: str,
+    random_state: int | None = None,
+) -> tuple[pd.DataFrame, SentenceTransformer, Language, GaussianMixture]:
+    data_file = ROOT_DIR / data_file_location
     return (
-        SentenceTransformer(model_name),
         pd.read_csv(data_file),
+        SentenceTransformer(transformer_model_name),
         spacy.load(nlp_name),
+        GaussianMixture(n_components=2, random_state=random_state),
     )
-
-
-def init_vec(
-    pos_loc: str | Path, neg_loc: str | Path, model: SentenceTransformer
-) -> list[Tensor | np.ndarray]:
-    pos_df = ROOT_DIR / pos_loc
-    neg_df = ROOT_DIR / neg_loc
-    pos_embeds = get_anchor_embeds(pos_df, model)
-    neg_embeds = get_anchor_embeds(neg_df, model)
-    axis_embed = (avg_vec(pos_embeds) + avg_vec(neg_embeds)) / 2
-    return [pos_embeds, neg_embeds, get_anchor_axis(pos_embeds, neg_embeds), axis_embed]
-
-
-def init_speech_embeds(
-    nlp: Language,
-    model: SentenceTransformer,
-    data: pd.DataFrame,
-    topic_speaker: str,
-    neutral_speaker: str,
-    topic_year: int | None = None,
-    neutral_year: int | None = None,
-) -> list[Tensor | np.ndarray]:
-    topic_embeds = get_speech_embeds(nlp, model, data, topic_speaker, topic_year)
-    neutral_embeds = get_speech_embeds(nlp, model, data, neutral_speaker, neutral_year)
-    return [topic_embeds, neutral_embeds]
